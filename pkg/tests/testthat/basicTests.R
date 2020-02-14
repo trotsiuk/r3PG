5822f3b7274ab8241b24b2b6a544457c47e7bc2d
florianhartig <florianhartig@users.noreply.github.com>
2020-02-14 21:14:03 +0100

    added test

--- pkg/tests/testthat/basicTests.R
+++ pkg/tests/testthat/basicTests.R
@@ -3,11 +3,18 @@ library(testthat)
 
 context("Basic Model runs work")
 
-test_that("ensureDHARMa", {
+test_that("basic model run", {
+  out <- run_3PG(
+    siteInputs = site_eum,
+    speciesInputs = species_eum,
+    forcingInputs = climate_eum,
+    parameterInputs = parameters_eum[,-1],
+    biasInputs = bias_eum[,-1],
+    settings = list(light_model = 1, transp_model = 1, phys_model = 1,
+                    correct_bias = 0, calculate_d13c = 0))
 
 
-
-
-
+  out_long <- transf_out( out )
 })
+
 
