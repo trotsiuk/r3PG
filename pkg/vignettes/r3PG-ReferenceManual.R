c954b4fa77c3a3bad26cf153bc0d67ce836a6272
florianhartig <florianhartig@users.noreply.github.com>
2020-03-24 20:17:46 +0100

    update ignore list

--- pkg/vignettes/r3PG-ReferenceManual.R
+++ pkg/vignettes/r3PG-ReferenceManual.R
@@ -1,19 +1,3 @@
-## ----setup, include=FALSE-----------------------------------------------------
+## ----setup, include=FALSE------------------------------------------------
 library(r3PG)
-
-## -----------------------------------------------------------------------------
-out <- run_3PG(
-  siteInputs = site_eum, 
-  speciesInputs = species_eum, 
-  forcingInputs = climate_eum, 
-  managementInputs = NULL,
-  parameterInputs = parameters_eum, 
-  biasInputs = bias_eum,
-  settings = list(light_model = 2, transp_model = 2, phys_model = 2, 
-    correct_bias = 0, calculate_d13c = 0),
-  df_out = FALSE)
-
-
-out_long <- transf_out( out )
-
 
