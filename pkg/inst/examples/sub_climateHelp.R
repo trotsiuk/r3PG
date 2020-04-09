0f3e5ae86bf59a910d858bdf4601dc057e1a8dbe
trotsiuk <vtrotsiuk@gmail.com>
2020-04-09 07:53:40 +0200

    Function to subset the climatic data to the format required for the analysis #14 #9

--- pkg/inst/examples/sub_climateHelp.R
+++ pkg/inst/examples/sub_climateHelp.R
@@ -1,4 +1,4 @@
 climate = matrix(rnorm(60), ncol = 5)
 colnames(climate) = c("tmp_min", "tmp_max", "prcp", "srad", "frost_days")
 
-gen_climate( climate, 3, 2)
+sub_climate( climate, from = '2000-04', to = '2010-11')
