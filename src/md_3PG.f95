c4ca4e2fd26e6ba9767e9ec00c9be58ad690407e
Forrester, David (L&W, Black Mountain) <David.Forrester@csiro.au>
2024-11-07 16:53:16 +1100

    Mortality option

--- src/md_3PG.f95
+++ src/md_3PG.f95
@@ -851,7 +851,7 @@ contains
             ! Used when mort_model = 2   !20241106
             prev_dbh(:) = dbh(:)
             prev_f_nutr(:) = f_nutr(:)
-            prev_f_tmp(:) = f_tmp(:)
+            prev_f_tmp(:,i) = f_tmp(:,i)
             prev_f_phys(:) = f_phys(:)
 
 
