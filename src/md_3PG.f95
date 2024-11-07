fbceeca09996d9b0c85044b9241c9470e56bd794
Forrester, David (L&W, Black Mountain) <David.Forrester@csiro.au>
2024-11-07 17:29:01 +1100

    Mortality option

--- src/md_3PG.f95
+++ src/md_3PG.f95
@@ -792,31 +792,32 @@ contains
 
                    else if ( mort_model .eq. int(2) ) then !20241106
 
-                !mort_thinn(i) = basal_area_prop(i) * (
-                !stems_n_ha(i) -
-                !(stems_n_ha(i) ^ (1 - betaN(i)) + Exp(beta0(i)) * (1 - betaN(i)) / (betaB(i) + 1) * (
-                !prev_dbh(i) ^ (betaB(i) + 1) * prev_f_nutr(i) ^ (betafN(i)) * prev_f_tmp(i) ^ (betafT(i)) * prev_f_phys(i) ^ (betafPhys(i)) -
-                !avDBH(i) ^ (betaB(i) + 1) * f_nutr(i) ^ (betafN(i)) * f_tmp(i) ^ (betafT(i))  * f_phys(i) ^ (betafPhys(i))
-                !)) ^ (1 / (1 - betaN(i)))
-                !)
 
 
+!mort_thinn(i) = basal_area_prop(i) * ( &
+!    stems_n_ha(i) - ( &
+!    stems_n_ha(i) ** (1 - betaN(i)) + exp(beta0(i)) * (1 - betaN(i)) / (betaB(i) + 1) * ( &
+!    prev_dbh(i) ** (betaB(i) + 1) * prev_f_nutr(i) ** (betafN(i)) * &
+!    prev_f_tmp(i) ** (betafT(i)) * prev_f_phys(i) ** (betafPhys(i)) - &
+!    avDBH(i) ** (betaB(i) + 1) * f_nutr(i) ** (betafN(i)) * &
+!    f_tmp(i) ** (betafT(i)) * f_phys(i) ** (betafPhys(i)) &
+!    ) &
+!    ) ** (1 / (1 - betaN(i))) )
 
 
+real :: temp1, temp2, temp3
 
+! Calculate intermediate terms to simplify the main expression
+temp1 = stems_n_ha(i) ** (1 - betaN(i))
+temp2 = exp(beta0(i)) * (1 - betaN(i)) / (betaB(i) + 1)
+temp3 = prev_dbh(i) ** (betaB(i) + 1) * prev_f_nutr(i) ** (betafN(i)) * &
+        prev_f_tmp(i) ** (betafT(i)) * prev_f_phys(i) ** (betafPhys(i)) - &
+        avDBH(i) ** (betaB(i) + 1) * f_nutr(i) ** (betafN(i)) * &
+        f_tmp(i) ** (betafT(i)) * f_phys(i) ** (betafPhys(i))
+
+! Calculate mort_thinn using the intermediate terms
 mort_thinn(i) = basal_area_prop(i) * ( &
-    stems_n_ha(i) - ( &
-    stems_n_ha(i) ** (1 - betaN(i)) + exp(beta0(i)) * (1 - betaN(i)) / (betaB(i) + 1) * ( &
-    prev_dbh(i) ** (betaB(i) + 1) * prev_f_nutr(i) ** (betafN(i)) * &
-    prev_f_tmp(i) ** (betafT(i)) * prev_f_phys(i) ** (betafPhys(i)) - &
-    avDBH(i) ** (betaB(i) + 1) * f_nutr(i) ** (betafN(i)) * &
-    f_tmp(i) ** (betafT(i)) * f_phys(i) ** (betafPhys(i)) &
-    ) &
-    ) ** (1 / (1 - betaN(i))) )
-
-!mort_thinn(i) =
-!   stems_n_ha(i) - (stems_n_ha(i) ** (1 - betaN(i)) + Exp(beta0(i)) * (1 - betaN(i)) / (betaB(i) + 1) * ( &
-!   prev_dbh(i) ** (betaB(i) + 1) * oldrelativeheight(i) ** (betarh(i)) - (avDBH(i)) ** (betaB(i) + 1) * relativeheight(i) ** (betarh(i)) ) ) ** (1 / (1 - betaN(i)))
+    stems_n_ha(i) - (temp1 + temp2 * temp3) ) ** (1 / (1 - betaN(i)))
 
 
 
