9c3be00a400504f59e1811ace2507a092c27b65d
Forrester, David (L&W, Black Mountain) <David.Forrester@csiro.au>
2024-11-08 14:23:13 +1100

    Mortality options

--- src/md_decl_const.f95
+++ src/md_decl_const.f95
@@ -35,7 +35,7 @@ module mod_decl_const
 
     real(kind=kind(0.0d0)), parameter :: beta0 = -17.4d0         !20241106
     real(kind=kind(0.0d0)), parameter :: betaB = 1.86d0             !20241106
-    real(kind=kind(0.0d0)), parameter :: betaN = 2.05d0          !20241106
+    real(kind=kind(0.0d0)), parameter :: betaN = 2.6d0          !20241106
     real(kind=kind(0.0d0)), parameter :: betafN = 0d0         !20241106
     real(kind=kind(0.0d0)), parameter :: betafT = 0d0         !20241106
     real(kind=kind(0.0d0)), parameter :: betafPhys = 0d0     !20241106
