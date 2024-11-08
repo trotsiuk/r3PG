6706f184aa3b6722954d9a8fff4eba9b8c98ce97
Forrester, David (L&W, Black Mountain) <David.Forrester@csiro.au>
2024-11-08 12:47:24 +1100

    Mortality options

--- src/md_3PG.f95
+++ src/md_3PG.f95
@@ -856,8 +856,8 @@ contains
                       mort_thinn(i) = basal_area_prop(i) * ( &
                                       (stems_n_total - ( &
                                       stems_n_total ** (1 - betaN) + Exp(beta0) * (1 - betaN) / (betaB + 1) * &
-                                      (prev_dbh_total ** (betaB + 1) * ave_lt_fN ** betafN * lt_fT ** betafT * lt_fPhys ** betafPhys - &
-                                      dbh_total ** (betaB + 1) * ave_lt_fN ** betafN * lt_fT ** betafT * lt_fPhys ** betafPhys) &
+                                      (prev_dbh_total ** (betaB + 1) * lt_fN ** betafN * lt_fT ** betafT * lt_fPhys ** betafPhys - &
+                                      dbh_total ** (betaB + 1) * lt_fN ** betafN * lt_fT ** betafT * lt_fPhys ** betafPhys) &
                                       ) ** (1 / (1 - betaN)) ))
 
             !ave_lt_fN = 0.0
