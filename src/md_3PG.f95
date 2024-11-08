f91b2a0939309e8651c2e8c76f2968b56791f574
Forrester, David (L&W, Black Mountain) <David.Forrester@csiro.au>
2024-11-08 11:38:16 +1100

    Mortality options

--- src/md_3PG.f95
+++ src/md_3PG.f95
@@ -859,12 +859,12 @@ contains
 
 
                       !mort_thinn(i) = basal_area_prop(i) * ( &
-                      !mort_thinn(i) = ( &
-                      !                (stems_n_total - ( &
-                      !                stems_n_total ** (1 - betaN) + Exp(beta0) * (1 - betaN) / (betaB + 1) * &
-                      !                (prev_dbh_total ** (betaB + 1) * lt_fN ** betafN * ln_fT ** betafT * ln_fPhys ** betafPhys - &
-                      !                dbh_total ** (betaB + 1) * lt_fN ** betafN * ln_fT ** betafT * ln_fPhys ** betafPhys) &
-                      !                ) ** (1 / (1 - betaN)) ))
+                      mort_thinn(i) = ( &
+                                      (stems_n_total - ( &
+                                      stems_n_total ** (1 - betaN) + Exp(beta0) * (1 - betaN) / (betaB + 1) * &
+                                      (prev_dbh_total ** (betaB + 1) * lt_fN ** betafN * ln_fT ** betafT * ln_fPhys ** betafPhys - &
+                                      dbh_total ** (betaB + 1) * lt_fN ** betafN * ln_fT ** betafT * ln_fPhys ** betafPhys) &
+                                      ) ** (1 / (1 - betaN)) ))
 
 
 
@@ -883,7 +883,7 @@ contains
 
 
 
-mort_thinn(i) = 0 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!mort_thinn(i) = 0 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
                           if( mort_thinn(i) > 0 ) then !20241106
