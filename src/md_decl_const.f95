c7213b3660fca221c05344dc37f331839e10132e
Forrester, David (L&W, Black Mountain) <David.Forrester@csiro.au>
2024-11-10 10:20:24 +1100

    Mortality options

--- src/md_decl_const.f95
+++ src/md_decl_const.f95
@@ -26,13 +26,6 @@ module mod_decl_const
 
 
 ! Parameters for self-thinning as rates of change                !20241106
-    !real(kind=kind(0.0d0)), parameter :: beta0 = -15.9d0         !20241106
-    !real(kind=kind(0.0d0)), parameter :: betaB = 1d0             !20241106
-    !real(kind=kind(0.0d0)), parameter :: betaN = 2.16d0          !20241106
-    !real(kind=kind(0.0d0)), parameter :: betafN = -1.3d0         !20241106
-    !real(kind=kind(0.0d0)), parameter :: betafT = -1.5d0         !20241106
-    !real(kind=kind(0.0d0)), parameter :: betafPhys = -2.16d0     !20241106
-
     real(kind=kind(0.0d0)), parameter :: beta0 = -20.4d0         !20241106
     real(kind=kind(0.0d0)), parameter :: betaB = 1.86d0             !20241106
     real(kind=kind(0.0d0)), parameter :: betaN = 2.6d0          !20241106
@@ -40,7 +33,7 @@ module mod_decl_const
     real(kind=kind(0.0d0)), parameter :: betafT = 0d0         !20241106
     real(kind=kind(0.0d0)), parameter :: betafPhys = 0d0     !20241106
 
-    integer :: mort_model = 2     ! 1 - 3PGpjs; 2 - 3PGmix       !20241106
+    integer :: mort_model = 1     ! 1 - 3PGpjs; 2 - 3PGmix       !20241106
 
 ! To be provided in the "species" inputs because they are species specific and not appropriate as site inputs
     real(kind=kind(0.0d0)), parameter :: lt_fN = 0.9d0             !20241106 long-term valules of modifiers
