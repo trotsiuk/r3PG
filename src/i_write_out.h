14365b1fef302e9fa29651c8699c48ace81c21bc
Forrester, David (L&W, Black Mountain) <David.Forrester@csiro.au>
2024-11-29 11:20:22 +1100

    Added extra test outputs

--- src/i_write_out.h
+++ src/i_write_out.h
@@ -108,6 +108,18 @@ output(ii,:,8,4) = mort_stress(:)
 ! DF add test output
 output(ii,:,8,5) = test_output        ! 20241106
 
+output(ii,:,8,6) = mort_thinn_total        ! 20241106
+output(ii,:,8,7) = ave_lt_fN        ! 20241129
+output(ii,:,8,8) = ave_lt_fT        ! 20241129
+output(ii,:,8,9) = ave_lt_fPhys     ! 20241129
+
+output(ii,:,8,10) = lt_fN(:)        ! 20241129
+output(ii,:,8,11) = lt_fT(:)        ! 20241129
+output(ii,:,8,12) = lt_fPhys(:)     ! 20241129
+
+output(ii,:,8,13) = dbh_total        ! 20241129
+output(ii,:,8,14) = stems_n_total     ! 20241129
+
 
 
 
