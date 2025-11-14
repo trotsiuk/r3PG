5deed583b653fbb78a460561ea30b309865c79a2
Forrester <for327@csiro.au>
2025-11-14 16:06:27 +1100

    Added lt_fT and lt_fPhys as outputs to provide 10-year averages.

--- src/i_write_out.h
+++ src/i_write_out.h
@@ -108,8 +108,10 @@ output(ii,:,8,5) = mort_thinn_total(:)        ! 20241106
 output(ii,:,8,6) = lt_fN_ave(:)        ! 20241129
 output(ii,:,8,7) = lt_fT_ave(:)        ! 20241129
 output(ii,:,8,8) = lt_fPhys_ave(:)     ! 20241129
-output(ii,:,8,9) = dbh_total(:)        ! 20241129
-output(ii,:,8,10) = stems_n_total(:)     ! 20241129
+output(ii,:,8,9) = lt_fT(:)        ! 20251114
+output(ii,:,8,10) = lt_fPhys(:)     ! 20251114
+output(ii,:,8,11) = dbh_total(:)        ! 20241129
+output(ii,:,8,12) = stems_n_total(:)     ! 20241129
 
 ! Wood Delta ------------------
 output(ii,:,9,1) = Gc_mol(:)
