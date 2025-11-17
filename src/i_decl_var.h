8c07a3c822692f90a63a0affb59670c5149f66d3
Forrester <for327@csiro.au>
2025-11-17 19:19:11 +1100

    add long term variables

--- src/i_decl_var.h
+++ src/i_decl_var.h
@@ -30,17 +30,29 @@ real(kind=kind(0.0d0)), dimension(n_sp) :: lt_fN      ! long-term value of fN mo
 !real(kind=kind(0.0d0)), dimension(n_sp) :: lt_fT      ! long-term value of fT modifier for a given species         !20241106 !20251114
 !real(kind=kind(0.0d0)), dimension(n_sp) :: lt_fPhys   ! long-term value of fPhysmod modifier for a given species   !20241106 !20251114
 
-! Long-term modifier values (runtime dimension)
-real(kind=8), allocatable :: lt_fT(:)         ! long-term temperature modifier     !20251114
-real(kind=8), allocatable :: lt_fPhys(:)      ! long-term physiological modifier   !20251114
+!! Long-term modifier values (runtime dimension)
+!real(kind=8), allocatable :: lt_fT(:)         ! long-term temperature modifier     !20251114
+!real(kind=8), allocatable :: lt_fPhys(:)      ! long-term physiological modifier   !20251114
+!! Long-term modifier state variables
+!integer, parameter :: n_sp_max = 200
+!logical :: lt_initialised(n_sp_max)           ! flags for cohorts                  !20251114
+!integer :: lt_mod_mths                        ! user-defined window (months)       !20251114
+!! Rolling-history arrays
+!real(kind=8), allocatable :: fT_hist(:,:)     ! fT history (n_sp x lt_mod_mths)    !20251114
+!real(kind=8), allocatable :: fPhys_hist(:,:)  ! fPhys history (n_sp x lt_mod_mths) !20251114
+!integer, allocatable :: hist_ptr(:)           ! circular buffer pointer (n_sp)     !20251114
+
+! Long-term modifier values
+real(kind=8), allocatable :: lt_fT(:)       ! long-term temperature modifier
+real(kind=8), allocatable :: lt_fPhys(:)    ! long-term physiological modifier
 ! Long-term modifier state variables
 integer, parameter :: n_sp_max = 200
-logical :: lt_initialised(n_sp_max)           ! flags for cohorts                  !20251114
-integer :: lt_mod_mths                        ! user-defined window (months)       !20251114
+logical :: lt_initialised(n_sp_max)         ! flags for cohorts
+integer :: lt_mod_mths                        ! user-provided window (months)
 ! Rolling-history arrays
-real(kind=8), allocatable :: fT_hist(:,:)     ! fT history (n_sp x lt_mod_mths)    !20251114
-real(kind=8), allocatable :: fPhys_hist(:,:)  ! fPhys history (n_sp x lt_mod_mths) !20251114
-integer, allocatable :: hist_ptr(:)           ! circular buffer pointer (n_sp)     !20251114
+real(kind=8), allocatable :: fT_hist(:,:)     ! fT history (n_sp x lt_mod_mths)
+real(kind=8), allocatable :: fPhys_hist(:,:)  ! fPhys history (n_sp x lt_mod_mths)
+integer, allocatable :: hist_ptr(:)           ! circular buffer pointer (n_sp)
 
 
 ! Climate ------------------------------
