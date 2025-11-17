4c7205154af3921c1acabd2b08a2cb9525f79689
Forrester <for327@csiro.au>
2025-11-18 09:09:28 +1100

    fixing long-term modifiers

--- src/md_3PG.f95
+++ src/md_3PG.f95
@@ -36,18 +36,6 @@ contains
         ! Output array
         real(kind=c_double), dimension(n_m,n_sp,11,20), intent(inout) :: output
 
-!-------------------------------------------------------------
-! Declarations (at top of subroutine, before any executable code)
-!-------------------------------------------------------------
-integer :: stat, s_index           ! simple integers
-integer :: m_lt                    ! loop variable for months
-integer(kind=8) :: approx_bytes
-real(kind=8) :: approx_mb
-
-! Long-term modifier variables are allocatable, declared in i_decl_var.h
-! lt_mod_mths should already be set by the user/input
-! n_sp comes from subroutine intent(in)
-
 
         ! Variables, Parameters, Constants
         include 'i_decl_var.h'
@@ -267,25 +255,25 @@ real(kind=8) :: approx_mb
 !fPhys_hist(:,:) = 0.0d0
 !hist_ptr(:) = 1
 
-!-------------------------------------------------------------
-! Initialization of long-term modifier arrays
-!-------------------------------------------------------------
-lt_initialised(:) = .false.  ! fixed-size logical array
+!-----------------------------
+! Long-term modifiers initialization
+!-----------------------------
+if (.not. allocated(lt_initialised)) allocate(lt_initialised(n_sp))
+lt_initialised(:) = .false.
 
-! Allocate only if not yet allocated
-if (.not. allocated(lt_fT)) then
-    allocate(lt_fT(n_sp))
-    allocate(lt_fPhys(n_sp))
-    allocate(fT_hist(n_sp, lt_mod_mths))
-    allocate(fPhys_hist(n_sp, lt_mod_mths))
-    allocate(hist_ptr(n_sp))
-end if
-
-! Initialize values
+if (.not. allocated(lt_fT)) allocate(lt_fT(n_sp))
 lt_fT(:) = 0.0d0
+
+if (.not. allocated(lt_fPhys)) allocate(lt_fPhys(n_sp))
 lt_fPhys(:) = 0.0d0
+
+if (.not. allocated(fT_hist)) allocate(fT_hist(n_sp, lt_mod_mths))
 fT_hist(:,:) = 0.0d0
+
+if (.not. allocated(fPhys_hist)) allocate(fPhys_hist(n_sp, lt_mod_mths))
 fPhys_hist(:,:) = 0.0d0
+
+if (.not. allocated(hist_ptr)) allocate(hist_ptr(n_sp))
 hist_ptr(:) = 1
 
 
