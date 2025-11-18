e91e9d544882af1318f0fe92713f2e52afcb749f
Forrester <for327@csiro.au>
2025-11-18 15:59:58 +1100

    fixing long term modifiers

--- src/md_3PG.f95
+++ src/md_3PG.f95
@@ -552,6 +552,30 @@ end do
 
 
 
+do i = 1, n_sp
+
+    ! Skip species not yet planted
+    if (age(month,i) < 0.d0) cycle
+
+    ! Update circular buffer index
+    hist_ptr(i) = mod(hist_ptr(i), lt_mod_mths) + 1
+
+    !---------------------------
+    ! 1) Temperature (lt_fT)
+    !---------------------------
+    fT_hist(i, hist_ptr(i)) = f_tmp(month, i)
+    lt_fT(i) = sum(fT_hist(i, 1:lt_mod_mths)) / real(lt_mod_mths, kind=8)
+
+    !---------------------------
+    ! 2) Physiological (lt_fPhys)
+    !---------------------------
+    fPhys_hist(i, hist_ptr(i)) = f_phys(i)
+    lt_fPhys(i) = sum(fPhys_hist(i, 1:lt_mod_mths)) / real(lt_mod_mths, kind=8)
+
+end do
+
+
+
 
 
 
