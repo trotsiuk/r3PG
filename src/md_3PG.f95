ca64cd0ddcde8ae708d75d4f219aa97c9bb7d8a9
Forrester <for327@csiro.au>
2025-11-21 13:23:59 +1100

    fixed m_apar

--- src/md_3PG.f95
+++ src/md_3PG.f95
@@ -562,7 +562,7 @@ end do
                 ! Calculate the absorbed PAR. If this is first month, then it will be only potential
                 call s_light_3pgmix ( n_sp, height(:), crown_length(:), crown_width(:), lai(:), stems_n(:), &
                     solar_rad(ii), CrownShape(:), k(:), gammaAPAR(:), adjSolarZenithAngle(month), daysInMonth(month), &
-                    apar(:), lai_above(:), fi(:), lambda_v(:), lambda_h(:), canopy_vol_frac(:), layer_id(:), lai_sa_ratio(:))
+                    apar(:), lai_above(:), fi(:), lambda_v(:), lambda_h(:), canopy_vol_frac(:), layer_id(:), lai_sa_ratio(:), m_apar(:))
 
                 VPD_sp(:) = vpd_day(ii) * Exp(lai_above(:) * (-Log(2.d0)) / cVPD(:))
             end if
@@ -1735,7 +1735,7 @@ end do
 
     subroutine s_light_3pgmix ( n_sp, height, crown_length, crown_width, lai, stems_n, solar_rad, &
         CrownShape, k, gammaAPAR, solarAngle,days_in_month, &
-        apar, lai_above, fi, lambda_v, lambda_h, canopy_vol_frac, layer_id, lai_sa_ratio)               !20251114
+        apar, lai_above, fi, lambda_v, lambda_h, canopy_vol_frac, layer_id, lai_sa_ratio, m_apar)               !20251114
 
         ! Subroutine calculate the apar for the mixed species forest
         ! It first allocate each species to a specific layer based on height and crown length
@@ -1804,8 +1804,7 @@ end do
 
 real(kind=kind(0.0d0)) :: height_wtav_LAI ! weighted average height of each cohort, where the weighting is by LAI !20251114
 real(kind=kind(0.0d0)), dimension(n_sp) :: height_rel_wt ! height of cohort relative to the weighted average (by LAI) height of all cohorts
-real(kind=kind(0.0d0)), dimension(n_sp) :: m_apar ! modifier to amplify light benefit to shorter cohorts
-
+real(kind=kind(0.0d0)), dimension(n_sp), intent(out) :: m_apar ! modifier to amplify light benefit to shorter cohorts
 
 
         ! initialization
