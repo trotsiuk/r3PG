7b6df70927500da7893d295cc3fd4006217077d1
Forrester <for327@csiro.au>
2026-02-02 15:43:00 +1100

    fixing height_rel

--- src/md_3PG.f95
+++ src/md_3PG.f95
@@ -2725,7 +2725,7 @@ end if
         real(kind=kind(0.0d0)), dimension(n_sp), intent(out) :: crown_length
         real(kind=kind(0.0d0)), dimension(n_sp), intent(out) :: crown_width
         real(kind=kind(0.0d0)), dimension(n_sp), intent(out) :: pFS
-        real(kind=kind(0.0d0)), dimension(15, n_sp), intent(out) :: bias_scale
+        real(kind=kind(0.0d0)), dimension(4, n_sp), intent(out) :: bias_scale
 
 
         ! Variables and parameters
