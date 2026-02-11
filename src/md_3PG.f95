56a0d80fed0295333504cf1721da102af912665b
Forrester <for327@csiro.au>
2026-02-12 08:59:57 +1100

    removed a bias calculation before thinning

--- src/md_3PG.f95
+++ src/md_3PG.f95
@@ -1842,31 +1842,12 @@ end do
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            do n = 1, b_n
-                competition_total = sum( wood_density(ii,:) * basal_area(:) )
-                call s_sizeDist_correct(n_sp, age(ii,:), stems_n(:), biom_tree(:), competition_total, lai(:), &
-                    height_model,  pars_i(69:88,:), pars_b, aWs(:), nWs(:), pfsPower(:), pfsConst(:), &
-                    dbh(:), basal_area(:), height(:), crown_length(:), crown_width(:), pFS(:), bias_scale(:,:) )
-            end do
+            !do n = 1, b_n
+            !    competition_total = sum( wood_density(ii,:) * basal_area(:) )
+            !    call s_sizeDist_correct(n_sp, age(ii,:), stems_n(:), biom_tree(:), competition_total, lai(:), &
+            !        height_model,  pars_i(69:88,:), pars_b, aWs(:), nWs(:), pfsPower(:), pfsConst(:), &
+            !        dbh(:), basal_area(:), height(:), crown_length(:), crown_width(:), pFS(:), bias_scale(:,:) )
+            !end do
 
             ! Volume and Volume increment
             ! This is done before thinning and mortality part
