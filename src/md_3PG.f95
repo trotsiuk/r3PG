3227287e213a82a0d9a5c0d7e2644fd993670a8c
Forrester <for327@csiro.au>
2026-02-11 16:22:40 +1100

    fixing

--- src/md_3PG.f95
+++ src/md_3PG.f95
@@ -725,14 +725,14 @@ close(400)
                    !        def_recover_t(i) = 0.0d0
                    !end if
 
-            !       Prune condition: def_type 1
-            i      f (def_type(i) == 1) then
+                  ! Prune condition: def_type 1
+                  if (def_type(i) == 1) then
                       if (age(ii,i) >= age_last_def_event(i) + 1.d0/12.d0) then
                           if (biom_foliage(i) >= biom_foliage_adj_pre_def(i)) then
                               def_recover_t(i) = 0.0d0
                           end if
                       end if
-            e      nd if
+                  end if
 
 
 
