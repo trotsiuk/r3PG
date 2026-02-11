7a4baf61df6c35e1a3d1df9804ecc47e49d3564e
Forrester <for327@csiro.au>
2026-02-11 21:02:01 +1100

    fixing

--- src/md_3PG.f95
+++ src/md_3PG.f95
@@ -2301,7 +2301,7 @@ end if
 
 
             ! Update dbh and basal area if there was self-thinning
-            if ( stems_loss_density(:) > 1.0e-6 ) then
+            if ( sum(stems_loss_density(:)) > 1.0e-6 ) then
 
                 biom_tree(:) = biom_stem(:) * 1000.d0 / stems_n(:)  ! kg/tree
                 dbh(:) = ( biom_tree(:) / aWs(:)) ** (1.d0 / nWs(:))
