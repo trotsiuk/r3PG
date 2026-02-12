f6fbeca6e61a7280dfe62e46c390076d970e9845
Forrester <for327@csiro.au>
2026-02-13 09:04:47 +1100

    fixing

--- src/md_3PG.f95
+++ src/md_3PG.f95
@@ -1490,9 +1490,9 @@ end if
             end if
             ! it is possible, especially for coppicing, that there is no change in stems_n but there is a reduction in stem mass
             if ( sum(biom_loss_stem_manag(:) + biom_loss_stem_def(:) + biom_loss_stem_stress(:)) > 1.0e-6 ) then
-                biom_tree(:) = biom_stem(:) * 1000.d0 / stems_n(:)  ! kg/tree
-                dbh(:) = ( biom_tree(:) / aWs(:)) ** (1.d0 / nWs(:))
-                basal_area(:) = dbh(:) ** 2.d0 / 4.d0 * Pi * stems_n(:) / 10000.d0
+                !biom_tree(:) = biom_stem(:) * 1000.d0 / stems_n(:)  ! kg/tree
+                !dbh(:) = ( biom_tree(:) / aWs(:)) ** (1.d0 / nWs(:))
+                !basal_area(:) = dbh(:) ** 2.d0 / 4.d0 * Pi * stems_n(:) / 10000.d0
             end if
 
             ! Update stand structure if there was a coppice event (the above lines many not capture this because coppicing does not always lead to stem loss)
