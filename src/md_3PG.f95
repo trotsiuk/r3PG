3ea3f5866547a116fbf26d616dcbc9ae939021c1
Forrester <for327@csiro.au>
2026-03-04 16:53:00 +1100

    fixing

--- src/md_3PG.f95
+++ src/md_3PG.f95
@@ -1322,7 +1322,7 @@ contains
                                do i = 1, n_sp
                                    ! protect against zero or tiny heights
                                    if (height_rel_wt(i) > 1.d-6) then
-                                       weight(i) = basal_area(i) * height_rel_wt(i)**(-4.d0) ! the 2.d0 defines the asymmetry of the weighting
+                                       weight(i) = basal_area(i) * height_rel_wt(i)**(-2.d0) ! the 2.d0 defines the asymmetry of the weighting
                                    else
                                        weight(i) = 0.d0
                                    end if
@@ -1404,7 +1404,7 @@ if (mort_model .eq. 3) then
                                do i = 1, n_sp
                                    ! protect against zero or tiny heights
                                    if (height_rel_wt(i) > 1.d-6) then
-                                       weight(i) = basal_area(i) * height_rel_wt(i)**(-4.d0) ! the 2.d0 defines the asymmetry of the weighting
+                                       weight(i) = basal_area(i) * height_rel_wt(i)**(-2.d0) ! the 2.d0 defines the asymmetry of the weighting
                                    else
                                        weight(i) = 0.d0
                                    end if
