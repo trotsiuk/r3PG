3a38251d4f2c1aa16b1ed868486d59642fa4663e
Forrester <for327@csiro.au>
2026-03-04 16:40:44 +1100

    fixing

--- src/md_3PG.f95
+++ src/md_3PG.f95
@@ -521,7 +521,7 @@ contains
                 call s_light_3pgmix ( n_sp, height(:), crown_length(:), crown_width(:), lai(:), stems_n(:), &
                     solar_rad(ii), CrownShape(:), k(:), gammaAPAR(:), adjSolarZenithAngle(month), daysInMonth(month), &
                     apar(:), lai_above(:), fi(:), lambda_v(:), lambda_h(:), canopy_vol_frac(:), layer_id(:), &
-                    lai_sa_ratio(:), m_apar(:))
+                    lai_sa_ratio(:), m_apar(:), height_rel_wt(:))
 
                 VPD_sp(:) = vpd_day(ii) * Exp(lai_above(:) * (-Log(2.d0)) / cVPD(:))
             end if
@@ -2024,7 +2024,7 @@ end if
 
     subroutine s_light_3pgmix ( n_sp, height, crown_length, crown_width, lai, stems_n, solar_rad, &
         CrownShape, k, gammaAPAR, solarAngle,days_in_month, &
-        apar, lai_above, fi, lambda_v, lambda_h, canopy_vol_frac, layer_id, lai_sa_ratio, m_apar)
+        apar, lai_above, fi, lambda_v, lambda_h, canopy_vol_frac, layer_id, lai_sa_ratio, m_apar, height_rel_wt)
 
         ! Subroutine calculate the apar for the mixed species forest
         ! It first allocate each species to a specific layer based on height and crown length
