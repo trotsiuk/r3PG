ea9c668b352823415098f26e10c1b37e6780d56e
Forrester <for327@csiro.au>
2026-03-04 17:14:21 +1100

    fixing

--- src/md_3PG.f95
+++ src/md_3PG.f95
@@ -1322,7 +1322,11 @@ contains
                                do i = 1, n_sp
                                    ! protect against zero or tiny heights
                                    if (height_rel_wt(i) > 1.d-6) then
-                                       weight(i) = basal_area(i) * height_rel_wt(i)**(-1.d0) ! the 2.d0 defines the asymmetry of the weighting (also change it below)
+                                        if (height_rel_wt(i) > 1.d-6) then
+                                            weight(i) = basal_area(i) * height_rel_wt(i)**(-1.d0) ! the exponent defines the asymmetry of the weighting (also change it below)
+                                        else if (height_rel_wt(i) > 1.d-6) then
+                                            weight(i) = basal_area(i) * height_rel_wt(i)**(-0.2d0) ! the exponent defines the asymmetry of the weighting (also change it below)
+                                        end
                                    else
                                        weight(i) = 0.d0
                                    end if
@@ -1404,7 +1408,11 @@ if (mort_model .eq. 3) then
                                do i = 1, n_sp
                                    ! protect against zero or tiny heights
                                    if (height_rel_wt(i) > 1.d-6) then
-                                       weight(i) = basal_area(i) * height_rel_wt(i)**(-1.d0) ! the 2.d0 defines the asymmetry of the weighting (also change it above)
+                                        if (height_rel_wt(i) > 1.d-6) then
+                                            weight(i) = basal_area(i) * height_rel_wt(i)**(-1.d0) ! the exponent defines the asymmetry of the weighting (also change it above)
+                                        else if (height_rel_wt(i) > 1.d-6) then
+                                            weight(i) = basal_area(i) * height_rel_wt(i)**(-0.2d0) ! the exponent defines the asymmetry of the weighting (also change it above)
+                                        end
                                    else
                                        weight(i) = 0.d0
                                    end if
